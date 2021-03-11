281639ceffc488a1107a73013eb79e2ee6821621
= <kevinlucas.tec.info@gmail.com>
2021-03-11 12:05:33 -0300

    Adicionando eventos aos elementos da interface

--- src/main/kotlin/ui/MainForm.java
+++ src/main/kotlin/ui/MainForm.java
@@ -2,6 +2,8 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainForm extends JFrame {
     private JPanel rootPanel;
@@ -18,5 +20,24 @@ public class MainForm extends JFrame {
         setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2);
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        setListeners();
+    }
+
+    private void setListeners() {
+        buttonNewContact.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new ContactForm();
+                dispose();
+            }
+        });
+
+        buttonRemoveContact.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 }
